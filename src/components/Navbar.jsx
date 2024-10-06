f624993093e4ac0b3e564ff1c38cc4c0fc0ae212
Karan Yadav <karan342772@gmail.com>
2024-10-06 18:19:33 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import logo from '../assets/logo.png'
+import logo from '/assets/logo.png'
 import { FaGithub, FaLinkedin,FaWhatsapp } from 'react-icons/fa'
 import { FaSquareXTwitter} from 'react-icons/fa6'
 function Navbar() {
