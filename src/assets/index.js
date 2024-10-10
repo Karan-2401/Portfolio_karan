b5322a04547a5019e5375531dcbcf95031571ad3
Karan Yadav <karan342772@gmail.com>
2024-10-10 15:04:24 +0530

    changed

--- src/assets/index.js
+++ src/assets/index.js
@@ -20,38 +20,45 @@ let projects = [
     image:p1,
     title:"Dice Game",
     descreption:"Developed an interactive single-player dice game using React for dynamic UI updates. Implemented game logic in JavaScript to generate random dice rolls, providing an engaging and unpredictable experience. The project incorporates modern web development practices with a responsive design using HTML CSS, and Tailwind for a seamless user experience across devices.",
+    link:"#"
     
   },
   
   {
     image:p3,
     title:"Gemini Clone",
-    descreption:"Developed a functional clone of Google Gemini using React and Tailwind CSS for a responsive and modern design. Integrated the Google API to enhance functionality, ensuring real-time data interaction. This project demonstrates advanced proficiency in React's component-based architecture and Tailwind's utility-first approach, providing a seamless user experience across devices. The clone accurately replicates the features and aesthetics of Google Gemini, showcasing API integration and modern web development techniques."
+    descreption:"Developed a functional clone of Google Gemini using React and Tailwind CSS for a responsive and modern design. Integrated the Google API to enhance functionality, ensuring real-time data interaction. This project demonstrates advanced proficiency in React's component-based architecture and Tailwind's utility-first approach, providing a seamless user experience across devices. The clone accurately replicates the features and aesthetics of Google Gemini, showcasing API integration and modern web development techniques.",
+    link:"#"
   },
   {
     image:p4,
     title:"Stcak Overflow Clone",
-    descreption:"Developed a full-featured Stack Overflow clone using React and React Router for seamless navigation across multiple pages. The project includes user-driven Q&A functionality, where users can post questions, submit answers, and upvote responses. Styled with Tailwind CSS for a modern and responsive layout, the clone replicates key features of Stack Overflow while ensuring a smooth, dynamic user experience through React. The implementation of React Router enhances the app's navigation, enabling multi-page routing for a real-world, scalable application."
+    descreption:"Developed a full-featured Stack Overflow clone using React and React Router for seamless navigation across multiple pages. The project includes user-driven Q&A functionality, where users can post questions, submit answers, and upvote responses. Styled with Tailwind CSS for a modern and responsive layout, the clone replicates key features of Stack Overflow while ensuring a smooth, dynamic user experience through React. The implementation of React Router enhances the app's navigation, enabling multi-page routing for a real-world, scalable application.",
+    link:"#"
   },
   {
     image:p2,
     title:"Nike Landing page",
-    descreption:"Built a visually striking landing page for Nike using React and Tailwind CSS. Implemented a modern, minimalistic design with fast load times and smooth user interactions. Leveraged React for dynamic UI updates and Tailwind CSS for utility-first styling, ensuring a seamless mobile-first experience. The page highlights Nike's latest products, focusing on user engagement and brand consistency.."
+    descreption:"Built a visually striking landing page for Nike using React and Tailwind CSS. Implemented a modern, minimalistic design with fast load times and smooth user interactions. Leveraged React for dynamic UI updates and Tailwind CSS for utility-first styling, ensuring a seamless mobile-first experience. The page highlights Nike's latest products, focusing on user engagement and brand consistency..",
+    link:"#"
   },
   {
     image:p5,
     title:"Movie Website",
-    descreption:"Developed a fully responsive movie website using HTML for structure, CSS for styling, and JavaScript along with jQuery for dynamic interactivity. The site allows users to browse through a collection of movies, view details such as ratings, descriptions, and trailers. jQuery was used to handle AJAX requests for real-time data updates, ensuring a smooth and engaging user experience. The design is optimized for performance and usability across devices, providing a seamless browsing experience"
+    descreption:"Developed a fully responsive movie website using HTML for structure, CSS for styling, and JavaScript along with jQuery for dynamic interactivity. The site allows users to browse through a collection of movies, view details such as ratings, descriptions, and trailers. jQuery was used to handle AJAX requests for real-time data updates, ensuring a smooth and engaging user experience. The design is optimized for performance and usability across devices, providing a seamless browsing experience",
+    link:"#"
   },
   {
     image:p6,
     title:"E-Commerce Website",
-    descreption:"Developed a feature-rich e-commerce website using HTML for content structure, CSS for styling, and Bootstrap for a responsive, mobile-friendly layout. Integrated JavaScript and jQuery to handle dynamic features such as product filtering, shopping cart functionality, and AJAX-based interactions. The site offers an intuitive user interface, smooth navigation, and a seamless shopping experience, optimized for performance across different devices."
+    descreption:"Developed a feature-rich e-commerce website using HTML for content structure, CSS for styling, and Bootstrap for a responsive, mobile-friendly layout. Integrated JavaScript and jQuery to handle dynamic features such as product filtering, shopping cart functionality, and AJAX-based interactions. The site offers an intuitive user interface, smooth navigation, and a seamless shopping experience, optimized for performance across different devices.",
+    link:"#"
   },
   {
     image:p7,
     title:"Secret Saver",
-    descreption:"Developed Secret Saver, a secure web application that allows users to create accounts for saving and managing phone numbers. The platform features a user-friendly interface for easy account management. Additionally, users can enhance their experience by pasting video URLs into a designated section to play videos directly on the site. This project showcases my skills in web development, focusing on user authentication, data management, and dynamic content integration. The application is designed with a clean, intuitive layout, ensuring a seamless user experience."
+    descreption:"Developed Secret Saver, a secure web application that allows users to create accounts for saving and managing phone numbers. The platform features a user-friendly interface for easy account management. Additionally, users can enhance their experience by pasting video URLs into a designated section to play videos directly on the site. This project showcases my skills in web development, focusing on user authentication, data management, and dynamic content integration. The application is designed with a clean, intuitive layout, ensuring a seamless user experience.",
+    link:"#"
   }
 ]
 export default projects;
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -9,10 +9,10 @@ function Projects() {
         {projects.map((x, index) => (
           <div key={index} className="mb-8 flex flex-wrap lg:justify-center">
             <motion.div whileInView={{opacity:1,x:0}} initial={{opacity:0,x:-100}} transition={{duration:0.5}} className="w-full lg:w-1/3">
-              <img src={x.image} alt="P1" className="mb-6 rounded lg:w-2/3" />
+              <a href={x.link}><img src={x.image} alt="P1" className="mb-6 rounded lg:w-2/3" /></a>
             </motion.div>
             <motion.div whileInView={{opacity:1,x:0}} initial={{opacity:0,x:100}} transition={{duration:0.5}} className="w-full max-w-xl lg:w-2/3">
-              <h1 className="mb-2 font-semibold">{x.title}</h1>
+              <a href={x.link}><h1 className="mb-2 font-semibold hover:text-purple-500">{x.title}</h1></a>
               <p className="mb-4 text-neutral-400">{x.descreption}</p>
               {x.tech}
             </motion.div>
